48d6ae2ef924506476312f5b7d6f6b6cdd08deb5
Daniel Dimi <eldanielzim@gmail.com>
2026-02-11 17:48:29 -0300

    Commit Atualizado

--- src/assets/produtos.js
+++ src/assets/produtos.js
@@ -5,14 +5,14 @@ export const produtos = [
     nome: "Gin Tônica Tradicional",
     categoria: "Gin",
     preco: 28.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 2,
     nome: "Gin Tropical",
     categoria: "Gin",
     preco: 32.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
 
   // VODKA
@@ -21,14 +21,14 @@ export const produtos = [
     nome: "Vodka com Energético",
     categoria: "Vodka",
     preco: 27.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 4,
     nome: "Moscow Mule",
     categoria: "Vodka",
     preco: 30.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
 
   // SEM ÁLCOOL
@@ -37,14 +37,14 @@ export const produtos = [
     nome: "Drink Tropical Sem Álcool",
     categoria: "Sem Álcool",
     preco: 18.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 6,
     nome: "Limonada Especial",
     categoria: "Sem Álcool",
     preco: 15.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
 
   // CERVEJA
@@ -53,48 +53,48 @@ export const produtos = [
     nome: "Heineken Long Neck",
     categoria: "Cerveja",
     preco: 14.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 8,
     nome: "Budweiser",
     categoria: "Cerveja",
     preco: 12.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 9,
     nome: "Cachaça Real",
     categoria: "Cachaça",
     preco: 15.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 10,
     nome: "Budweiser",
     categoria: "Cerveja",
     preco: 12.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 10,
     nome: "Budweiser",
     categoria: "Cerveja",
     preco: 12.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },
   {
     id: 10,
     nome: "Budweiser",
     categoria: "Cerveja",
     preco: 12.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   },{
     id: 10,
     nome: "Budweiser",
     categoria: "Cerveja",
     preco: 12.90,
-    imagem: '/images/gin.jpg'
+    imagem: '/images/Gin.jpg'
   }
 ]
 
